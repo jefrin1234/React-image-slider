af2d2ae52bb26959df8d6b7a87587f42893aff5e
jefrin james <jamesjefrin13@gmail.com>
2024-06-16 16:41:38 +0530

    added all files

---
+++ my-react-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import ImageSlider from './Image-slider/Image-Slider';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      /> 
+    </div>
+  );
+}
+
+export default App;
